d14190bd616bb1f7ca7668baeb8de0d29c78c966
ripleycmd <natalia.marek@outlook.com>
2020-03-16 23:29:39 +0000

    Create function to convert given password to SHA1, and a function calling pwned passwords API + adding a runtime error information

--- password.py
+++ password.py
@@ -1,8 +1,24 @@
 import requests
-# import hashlib
+import hashlib            # built in module 
 # import sys
 
 
-url = 'https://api.pwnedpasswords.com/range/' + 'password123'
+url = 'https://api.pwnedpasswords.com/range/' + '5BAA6' #by using first 5 characters of hashed password we make sure this API will never know our password
 res = requests.get(url)
 print(res)
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + '5BAA6' #by using first 5 characters of hashed password we make sure this API will never know our password
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code} check the API and try again')
+
+
+def pwned_api_check(password):
+    #print(hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
+    #checking if the password exists in the API response
+    sha1password = hashlib.sha1(password.encode('utf-8'))
+    return sha1password
+
+
+pwned_api_check('123')
